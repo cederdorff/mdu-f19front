134e37bd7a4ffebd293a04ebd78c6681aee0d20c
Rasmus Cederdorff <rasmus@cederdorff.com>
2020-04-02 16:08:27 +0200

    chart js solutions added

---
+++ chart_js_spa_modules_auth/pages/chart.js
@@ -0,0 +1,113 @@
+import sustainabilityDataService from "../services/sustainabilityData.js";
+import authService from "../services/auth.js";
+
+export default class ChartPage {
+  constructor() {
+    this.template();
+
+  }
+
+  async init() {
+    let uid = authService.authUser.uid;
+    let data = await sustainabilityDataService.getPreparedDataByUid(uid); // getting prepared data from the service
+    // call append functions with the dataset: data
+    this.appendCowsChart(data);
+    this.appendCarbonFootprint(data);
+    this.appendMilkProduction(data);
+  }
+
+  template() {
+    document.getElementById('content').innerHTML += /*html*/ `
+      <section id="charts" class="page">
+        <header class="topbar">
+          <h2>Charts</h2>
+        </header>
+        <h3>Herd - Number of Cows</h3>
+        <canvas id="cows"></canvas>
+        <h3>Carbon Footprint</h3>
+        <canvas id="carbonFootprint"></canvas>
+        <h3>Milk Production pr cow</h3>
+        <canvas id="milkProduction"></canvas>
+      </section>
+    `;
+  }
+
+  //appending the chart
+  appendCowsChart(data) {
+    // generate chart
+    let chartContainer = document.getElementById("cows");
+    let chart = new Chart(chartContainer, {
+      type: 'line',
+      data: {
+        datasets: [{
+          data: data.numberOfCows,
+          label: 'Number of Cows',
+          fill: false,
+          borderColor: "#e755ba",
+          backgroundColor: "#e755ba",
+          pointBackgroundColor: "#55bae7",
+          pointBorderColor: "#55bae7",
+          pointHoverBackgroundColor: "#55bae7",
+          pointHoverBorderColor: "#55bae7",
+        }],
+        labels: data.years
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              min: (Math.min(...data.numberOfCows) - 5),
+              max: (Math.max(...data.numberOfCows) + 1)
+            }
+          }]
+        }
+      }
+    });
+  }
+
+  //appending the chart
+  appendCarbonFootprint(data) {
+    // generate chart
+    let chartContainer = document.getElementById('carbonFootprint');
+    let chart = new Chart(chartContainer, {
+      type: 'line',
+      data: {
+        datasets: [{
+          data: data.carbonFootprint,
+          label: 'Carbon Footprint WholeFarm',
+          fill: false,
+          borderColor: "#e755ba",
+          backgroundColor: "#e755ba",
+          pointBackgroundColor: "#55bae7",
+          pointBorderColor: "#55bae7",
+          pointHoverBackgroundColor: "#55bae7",
+          pointHoverBorderColor: "#55bae7",
+        }],
+        labels: data.years
+      }
+    });
+  }
+
+  //appending the chart
+  appendMilkProduction(data) {
+    // generate chart
+    let chartContainer = document.getElementById('milkProduction');
+    let chart = new Chart(chartContainer, {
+      type: 'line',
+      data: {
+        datasets: [{
+          data: data.milkProduction,
+          label: 'Milk Production',
+          fill: false,
+          borderColor: "#e755ba",
+          backgroundColor: "#e755ba",
+          pointBackgroundColor: "#55bae7",
+          pointBorderColor: "#55bae7",
+          pointHoverBackgroundColor: "#55bae7",
+          pointHoverBorderColor: "#55bae7",
+        }],
+        labels: data.years
+      }
+    });
+  }
+}
---
+++ chart_js_spa_modules_auth/pages/chartCompare.js
@@ -0,0 +1,150 @@
+import sustainabilityDataService from "../services/sustainabilityData.js";
+import authService from "../services/auth.js";
+
+export default class ChartComparePage {
+  constructor() {
+    this.template();
+  }
+
+  async init() {
+    // user 1 - auth user 
+    let uid = authService.authUser.uid;
+    let data = await sustainabilityDataService.getPreparedDataByUid(uid); // getting prepared data from the service
+    // user 2 - Ep7o7EToQtZzdKnEDy2ahirFHc43
+    let uidCompare = "Ep7o7EToQtZzdKnEDy2ahirFHc43"; // matching an uid from the database
+    let dataCompare = await sustainabilityDataService.getPreparedDataByUid(uidCompare); // getting prepared data from the service
+    // call the append functions with two datasets: data & compare
+    this.appendCowsChart(data, dataCompare);
+    this.appendCarbonFootprint(data, dataCompare);
+    this.appendMilkProduction(data, dataCompare);
+  }
+
+  template() {
+    document.getElementById('content').innerHTML += /*html*/ `
+      <section id="chartCompare" class="page">
+        <header class="topbar">
+          <h2>Charts Two datasets</h2>
+        </header>
+        <h3>Herd - Number of Cows</h3>
+        <canvas id="cowsCompare"></canvas>
+        <h3>Carbon Footprint</h3>
+        <canvas id="carbonFootprintCompare"></canvas>
+        <h3>Milk Production pr cow</h3>
+        <canvas id="milkProductionCompare"></canvas>
+      </section>
+    `;
+  }
+
+  //appending the chart using two datasets, given as argument: data & dataCompare
+  appendCowsChart(data, dataCompare) {
+    // generate chart
+    let chartContainer = document.getElementById("cowsCompare");
+    let chart = new Chart(chartContainer, {
+      type: 'line',
+      data: {
+        datasets: [{
+            data: data.numberOfCows,
+            label: 'Number of Cows',
+            fill: false,
+            borderColor: "#e755ba",
+            backgroundColor: "#e755ba",
+            pointBackgroundColor: "#55bae7",
+            pointBorderColor: "#55bae7",
+            pointHoverBackgroundColor: "#55bae7",
+            pointHoverBorderColor: "#55bae7",
+          },
+          // secobd dataset - second user: Ep7o7EToQtZzdKnEDy2ahirFHc43
+          {
+            label: 'User: Ep7o7EToQtZzdKnEDy2ahirFHc43',
+            data: dataCompare.numberOfCows,
+            fill: false,
+            borderColor: "#55bae7",
+            backgroundColor: "#55bae7",
+            pointBackgroundColor: "#e755ba",
+            pointBorderColor: "#e755ba",
+            pointHoverBackgroundColor: "#e755ba",
+            pointHoverBorderColor: "#e755ba",
+            type: 'line'
+          }
+        ],
+        labels: data.years
+      }
+    });
+  }
+
+  //appending the chart using two datasets, given as argument: data & dataCompare
+  appendCarbonFootprint(data, dataCompare) {
+    // generate chart
+    let chartContainer = document.getElementById('carbonFootprintCompare');
+    let chart = new Chart(chartContainer, {
+      type: 'line',
+      data: {
+        datasets: [{
+            data: data.carbonFootprint,
+            label: 'Carbon Footprint WholeFarm',
+            fill: false,
+            borderColor: "#e755ba",
+            backgroundColor: "#e755ba",
+            pointBackgroundColor: "#55bae7",
+            pointBorderColor: "#55bae7",
+            pointHoverBackgroundColor: "#55bae7",
+            pointHoverBorderColor: "#55bae7",
+          },
+          // secobd dataset - second user: Ep7o7EToQtZzdKnEDy2ahirFHc43
+          {
+            label: 'User: Ep7o7EToQtZzdKnEDy2ahirFHc43',
+            data: dataCompare.carbonFootprint,
+            fill: false,
+            borderColor: "#55bae7",
+            backgroundColor: "#55bae7",
+            pointBackgroundColor: "#e755ba",
+            pointBorderColor: "#e755ba",
+            pointHoverBackgroundColor: "#e755ba",
+            pointHoverBorderColor: "#e755ba",
+            type: 'line'
+          }
+        ],
+        labels: data.years
+      }
+    });
+  }
+
+  //appending the chart using two datasets, given as argument: data & dataCompare
+  appendMilkProduction(data, dataCompare) {
+    // generate chart
+    let chartContainer = document.getElementById('milkProductionCompare');
+    let chart = new Chart(chartContainer, {
+      type: 'line',
+      data: {
+        datasets: [
+          // first dataset - auth user
+          {
+            data: data.milkProduction,
+            label: 'User: authUser',
+            fill: false,
+            borderColor: "#e755ba",
+            backgroundColor: "#e755ba",
+            pointBackgroundColor: "#55bae7",
+            pointBorderColor: "#55bae7",
+            pointHoverBackgroundColor: "#55bae7",
+            pointHoverBorderColor: "#55bae7",
+          },
+          // secobd dataset - second user: Ep7o7EToQtZzdKnEDy2ahirFHc43
+          {
+            label: 'User: Ep7o7EToQtZzdKnEDy2ahirFHc43',
+            data: dataCompare.milkProduction,
+            fill: false,
+            borderColor: "#55bae7",
+            backgroundColor: "#55bae7",
+            pointBackgroundColor: "#e755ba",
+            pointBorderColor: "#e755ba",
+            pointHoverBackgroundColor: "#e755ba",
+            pointHoverBorderColor: "#e755ba",
+            type: 'line'
+          }
+        ],
+        labels: data.years
+      }
+    });
+  }
+}
