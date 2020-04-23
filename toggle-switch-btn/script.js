3700fa8e413baa6d51aa32e2c715e60c4e6274c0
Rasmus Cederdorff <rasmus@cederdorff.com>
2020-04-23 13:05:55 +0200

    toogle customised

--- toggle-switch-btn/script.js
+++ toggle-switch-btn/script.js
@@ -1,7 +1,15 @@
-function changeEvent(element) {
+function changeEventOne(element) {
     console.log(element);
     console.log(element.checked);
     if (element.checked) {
         alert("Yeah, it's on!")
+    }
+}
+
+function changeEventTwo(element) {
+    console.log(element);
+    console.log(element.checked);
+    if (!element.checked) {
+        alert("Oh, turning off!")
     }
 }
