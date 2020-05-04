1035ea5927ee2393c2b33da50e44b97ceadf210b
Rasmus Cederdorff <rasmus@cederdorff.com>
2020-05-04 23:21:58 +0200

    vue-cli projects added

--- vue-cli-contact-component/src/components/UserList.vue
+++ vue-cli-contact-component/src/components/UserList.vue
@@ -1,29 +1,29 @@
 <template>
-    <section>
-        <article v-for="user in users" :key="user.name">
-            <h3>{{user.name}}</h3>
-            <a :href="`mailto:${user.mail}`">{{user.mail}}</a>
-        </article>
-    </section>
+  <section>
+    <article v-for="user in users" :key="user.name">
+      <h3>{{user.name}}</h3>
+      <a :href="`mailto:${user.mail}`">{{user.mail}}</a>
+    </article>
+  </section>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       users: [
-        { name: 'Birgitte', mail: 'bki@eaaa.dk' },
-        { name: 'Lykke', mail: 'lyda@eaaa.dk' },
-        { name: 'Rasmus', mail: 'race@eaaa.dk' }
+        { name: "Birgitte", mail: "bki@eaaa.dk" },
+        { name: "Lykke", mail: "lyda@eaaa.dk" },
+        { name: "Rasmus", mail: "race@eaaa.dk" }
       ]
-    }
+    };
   }
-}
+};
 </script>
 
 <style scoped>
-    section{
-        display: grid;
-        grid-template-columns: 33.33% 33.33% 33.33%;
-    }
+section {
+  display: grid;
+  grid-template-columns: 33.33% 33.33% 33.33%;
+}
 </style>
